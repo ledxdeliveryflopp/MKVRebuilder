ed1acbd34575ccf9e17e7cd48b480ee5abe0b757
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-05 15:43:11 +0700

    Добавил автоматический перевод интерфейса на русский

---
+++ dev/ts/ru_RU.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>ConsoleWidget</name>
+    <message>
+        <location filename="src/console/widget.py" line="51"/>
+        <source>extract subtitle: Not started</source>
+        <translation>извлечение субтитров: не начато</translation>
+    </message>
+    <message>
+        <location filename="src/console/widget.py" line="52"/>
+        <source>convert dts to ac3: Not started</source>
+        <translation>конвертация dts в ac3: не начато</translation>
+    </message>
+    <message>
+        <location filename="src/console/widget.py" line="53"/>
+        <source>build new mkv: Not started</source>
+        <translation>Сборка нового mkv: не начата</translation>
+    </message>
+    <message>
+        <location filename="src/console/widget.py" line="116"/>
+        <location filename="src/console/widget.py" line="117"/>
+        <location filename="src/console/widget.py" line="143"/>
+        <source>Skipped</source>
+        <translation>Пропуск</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/main/window.py" line="36"/>
+        <source>Set source file</source>
+        <translation>Выбрать исходный файл</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="37"/>
+        <source>Set output directory</source>
+        <translation>Выбрать директорию  длясохранения нового файла</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="39"/>
+        <source>Set temp directory</source>
+        <translation>Выбрать директорию для временных файлов</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="40"/>
+        <source>Configurate ac3 file</source>
+        <translation>Настройки ac3</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="41"/>
+        <source>Update subtitles</source>
+        <translation>Обновить субтитры</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="43"/>
+        <location filename="src/main/window.py" line="44"/>
+        <location filename="src/main/window.py" line="48"/>
+        <source>Empty</source>
+        <translation>Не выбрано</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="144"/>
+        <source>Select source</source>
+        <translation>Выберите исходный файл</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="163"/>
+        <source>Select target</source>
+        <translation>Выберите директорию для сохранения</translation>
+    </message>
+    <message>
+        <location filename="src/main/window.py" line="173"/>
+        <source>Select temp dir</source>
+        <translation>Выберите директорию для временных файлов</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="src/rebuilder/widget.py" line="30"/>
+        <source>Select bitrate</source>
+        <translation>Выбор битрейта</translation>
+    </message>
+    <message>
+        <location filename="src/rebuilder/widget.py" line="31"/>
+        <source>Start</source>
+        <translation>Начать конвертацию и сборку</translation>
+    </message>
+    <message>
+        <location filename="src/rebuilder/widget.py" line="54"/>
+        <source>Rebuild mkv with 1 ac3 track</source>
+        <translation>Пересобрать файл с нужной дорожкой ac3</translation>
+    </message>
+</context>
+</TS>
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
 import sys
 
+from PySide6.QtCore import QTranslator, QLocale
 from PySide6.QtWidgets import QApplication
 from loguru import logger
 
@@ -9,8 +9,20 @@ from src.settings.config import ini_settings
 
 
 @logger.catch
+def set_up_translator() -> QTranslator:
+    """Установка перевода"""
+    translator = QTranslator()
+    system_language = QLocale.system().name()
+    if system_language:
+        translator.load(f"static/localization/{system_language}.qm")
+    return translator
+
+
+@logger.catch
 def set_up_app() -> None:
     app = QApplication(sys.argv)
+    translator = set_up_translator()
+    app.installTranslator(translator)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
@@ -21,6 +33,7 @@ if __name__ == '__main__':
                format="{time:DD-MM-YYYY at HH:mm:ss} | {level} | {message}")
     ini_settings.set_base_ini_config()
     set_up_app()
+
 
 
 
