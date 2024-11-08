fd61619ead5723f07fa486304a6b5d9f256bf5cd
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-08 16:56:46 +0700

    Обновил перевод.

--- dev/ts/ru_RU.ts
+++ dev/ts/ru_RU.ts
@@ -4,64 +4,79 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="src/main/window.py" line="45"/>
+        <location filename="../../src/main/window.py" line="54"/>
         <source>Set source file</source>
         <translation>Выбор исходного файла</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="47"/>
+        <location filename="../../src/main/window.py" line="56"/>
         <source>Set output directory</source>
         <translation>Выбор директории сохранения</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="48"/>
+        <location filename="../../src/main/window.py" line="57"/>
         <source>Set temp directory</source>
         <translation>Выбор директории временных файлов</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="49"/>
+        <location filename="../../src/main/window.py" line="58"/>
         <source>Choose bitrate</source>
         <translation>Выбор битрейта</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="50"/>
+        <location filename="../../src/main/window.py" line="59"/>
         <source>Start rebuilding mkv</source>
         <translation>Начать сборку mkv</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="51"/>
+        <location filename="../../src/main/window.py" line="60"/>
         <source>Update subtitles</source>
         <translation>Вырезать субтитры</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="53"/>
-        <location filename="src/main/window.py" line="57"/>
-        <location filename="src/main/window.py" line="61"/>
+        <location filename="../../src/main/window.py" line="62"/>
+        <location filename="../../src/main/window.py" line="68"/>
+        <location filename="../../src/main/window.py" line="74"/>
         <source>Empty</source>
         <translation>Не выбрано</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="182"/>
+        <location filename="../../src/main/window.py" line="81"/>
+        <source>Black theme</source>
+        <translation>Темная тема</translation>
+    </message>
+    <message>
+        <location filename="../../src/main/window.py" line="84"/>
+        <source>Standard theme</source>
+        <translation>Стандартная тема</translation>
+    </message>
+    <message>
+        <location filename="../../src/main/window.py" line="88"/>
+        <source>Style</source>
+        <translation>Стили</translation>
+    </message>
+    <message>
+        <location filename="../../src/main/window.py" line="250"/>
         <source>Select source</source>
         <translation>Выбор исходного файла</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="205"/>
+        <location filename="../../src/main/window.py" line="277"/>
         <source>Select target</source>
         <translation>Выбор директории сохранения</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="218"/>
+        <location filename="../../src/main/window.py" line="292"/>
         <source>Select temp dir</source>
         <translation>Выбор директории временных файлов</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="253"/>
+        <location filename="../../src/main/window.py" line="311"/>
         <source>Start rebuild mkv with {codec}</source>
         <translation>Начать сборку mkv с кодеком</translation>
     </message>
     <message>
-        <location filename="src/main/window.py" line="271"/>
+        <location filename="../../src/main/window.py" line="329"/>
         <source>Temp dir/output dir/source file/sound track dont selected</source>
         <translation>Не установлена папка временных файлов или исходный файл или звуковая дорожка</translation>
     </message>
@@ -69,79 +84,56 @@
 <context>
     <name>RebuilderWidget</name>
     <message>
-        <location filename="src/rebuilder/widget.py" line="68"/>
         <source>build new mkv: Not started</source>
-        <translation>Сборка mkv: Не начата</translation>
+        <translation type="vanished">Сборка mkv: Не начата</translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="71"/>
-        <location filename="src/rebuilder/widget.py" line="77"/>
         <source>Skip</source>
-        <translation>Конвертация dts в ac3: Пропуск</translation>
+        <translation type="vanished">Конвертация dts в ac3: Пропуск</translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="73"/>
         <source>convert dts to ac3: Not started</source>
-        <translation>Конвертация dts в ac3: Не начата</translation>
+        <translation type="vanished">Конвертация dts в ac3: Не начата</translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="75"/>
         <source>extract subtitle: Not started</source>
-        <translation>Извлечение субтитров: Не начато</translation>
+        <translation type="vanished">Извлечение субтитров: Не начато</translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="171"/>
-        <location filename="src/rebuilder/widget.py" line="189"/>
-        <location filename="src/rebuilder/widget.py" line="207"/>
         <source>extract sound: {line}</source>
-        <translation>Извлечение звуковой дорожки: </translation>
+        <translation type="vanished">Извлечение звуковой дорожки: </translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="175"/>
         <source>extract sound, progress 100: %.3f minutes</source>
-        <translation>Извлечение звуковой дорожки, прогресс 100: </translation>
+        <translation type="vanished">Извлечение звуковой дорожки, прогресс 100: </translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="193"/>
-        <location filename="src/rebuilder/widget.py" line="211"/>
         <source>extracting sound, progress 100: %.3f minutes</source>
-        <translation>Извлечение звуковой дорожки, прогресс 100: </translation>
+        <translation type="vanished">Извлечение звуковой дорожки, прогресс 100: </translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="225"/>
         <source>extract subtitle: {line}</source>
-        <translation>Извлечение субтитров: </translation>
+        <translation type="vanished">Извлечение субтитров: </translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="229"/>
         <source>extracting subtitles, progress 100: %.3f minutes</source>
-        <translation>Извлечение субтитров, прогресс 100: </translation>
+        <translation type="vanished">Извлечение субтитров, прогресс 100: </translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="242"/>
         <source>converting dts to ac3</source>
-        <translation>Конвертация dts в ac3</translation>
+        <translation type="vanished">Конвертация dts в ac3</translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="248"/>
         <source>dts to ac3, progress 100: %.3f minutes</source>
-        <translation>Конвертация dts в ac3, прогресс 100:</translation>
+        <translation type="vanished">Конвертация dts в ac3, прогресс 100:</translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="262"/>
-        <location filename="src/rebuilder/widget.py" line="274"/>
-        <location filename="src/rebuilder/widget.py" line="293"/>
-        <location filename="src/rebuilder/widget.py" line="306"/>
         <source>rebuild mkv: {line}</source>
-        <translation>Сборка mkv: </translation>
+        <translation type="vanished">Сборка mkv: </translation>
     </message>
     <message>
-        <location filename="src/rebuilder/widget.py" line="266"/>
-        <location filename="src/rebuilder/widget.py" line="278"/>
-        <location filename="src/rebuilder/widget.py" line="297"/>
-        <location filename="src/rebuilder/widget.py" line="310"/>
         <source>rebuild mkv, progress 100: %.3f minutes</source>
-        <translation>Сборка mkv:, прогресс 100:</translation>
+        <translation type="vanished">Сборка mkv:, прогресс 100:</translation>
     </message>
 </context>
 </TS>
