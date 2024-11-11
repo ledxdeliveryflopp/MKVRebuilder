2f5411983334c20d63d73cfeb6bc2805593d5926
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-11 22:39:12 +0700

    Дополнен перевод.
    1. Добавлено описание пропущенного этапа при попытке сборки.

--- dev/ts/ru_RU.ts
+++ dev/ts/ru_RU.ts
@@ -4,81 +4,103 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../src/main/window.py" line="54"/>
+        <location filename="../../src/main/window.py" line="62"/>
         <source>Set source file</source>
         <translation>Выбор исходного файла</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="56"/>
+        <location filename="../../src/main/window.py" line="63"/>
         <source>Set output directory</source>
         <translation>Выбор директории сохранения</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="57"/>
+        <location filename="../../src/main/window.py" line="64"/>
         <source>Set temp directory</source>
         <translation>Выбор директории временных файлов</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="58"/>
+        <location filename="../../src/main/window.py" line="65"/>
         <source>Choose bitrate</source>
         <translation>Выбор битрейта</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="59"/>
+        <location filename="../../src/main/window.py" line="66"/>
         <source>Start rebuilding mkv</source>
         <translation>Начать сборку mkv</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="60"/>
+        <location filename="../../src/main/window.py" line="67"/>
         <source>Update subtitles</source>
         <translation>Вырезать субтитры</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="62"/>
-        <location filename="../../src/main/window.py" line="68"/>
-        <location filename="../../src/main/window.py" line="74"/>
+        <location filename="../../src/main/window.py" line="69"/>
+        <location filename="../../src/main/window.py" line="75"/>
+        <location filename="../../src/main/window.py" line="81"/>
         <source>Empty</source>
         <translation>Не выбрано</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="81"/>
+        <location filename="../../src/main/window.py" line="90"/>
         <source>Black theme</source>
         <translation>Темная тема</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="84"/>
+        <location filename="../../src/main/window.py" line="93"/>
         <source>Standard theme</source>
         <translation>Стандартная тема</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="88"/>
+        <location filename="../../src/main/window.py" line="97"/>
         <source>Style</source>
         <translation>Стили</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="250"/>
+        <location filename="../../src/main/window.py" line="285"/>
         <source>Select source</source>
         <translation>Выбор исходного файла</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="277"/>
+        <location filename="../../src/main/window.py" line="319"/>
         <source>Select target</source>
         <translation>Выбор директории сохранения</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="292"/>
+        <location filename="../../src/main/window.py" line="341"/>
         <source>Select temp dir</source>
         <translation>Выбор директории временных файлов</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="311"/>
-        <source>Start rebuild mkv with {codec}</source>
-        <translation>Начать сборку mkv с кодеком</translation>
+        <location filename="../../src/main/window.py" line="387"/>
+        <source>Source file: dont selected</source>
+        <translation>Исходный файл: не выбран</translation>
     </message>
     <message>
-        <location filename="../../src/main/window.py" line="329"/>
+        <location filename="../../src/main/window.py" line="391"/>
+        <source>Output dir: dont selected</source>
+        <translation>Директория сохранения: не выбрана</translation>
+    </message>
+    <message>
+        <location filename="../../src/main/window.py" line="395"/>
+        <source>Temp dir: dont selected</source>
+        <translation>Временная папка: не выбрана</translation>
+    </message>
+    <message>
+        <location filename="../../src/main/window.py" line="399"/>
+        <source>Sound track: dont selected</source>
+        <translation>Звуковая дорожка: не выбрана</translation>
+    </message>
+    <message>
+        <source>Start rebuild mkv with</source>
+        <translation type="vanished">Начать сборку vmk с кодеком:</translation>
+    </message>
+    <message>
+        <source>Start rebuild mkv with {codec}</source>
+        <translation type="vanished">Начать сборку mkv с кодеком</translation>
+    </message>
+    <message>
         <source>Temp dir/output dir/source file/sound track dont selected</source>
-        <translation>Не установлена папка временных файлов или исходный файл или звуковая дорожка</translation>
+        <translation type="vanished">Не установлена папка временных файлов или исходный файл или звуковая дорожка</translation>
     </message>
 </context>
 <context>
