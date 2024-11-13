de968f8ed28ad1f6b78f44b3df86dc26ebe4df2f
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-13 21:24:04 +0700

    Убран класс дебаг-потока из менеджера потоков.

--- src/settings/thread_manager.py
+++ src/settings/thread_manager.py
@@ -1,8 +1,6 @@
-import time
 from dataclasses import dataclass
 
-import psutil
-from PySide6.QtCore import QThreadPool, Signal, QThread
+from PySide6.QtCore import QThreadPool
 
 
 @dataclass
@@ -18,22 +16,3 @@ class ThreadManager:
         """Возврат колличества доступных потоков в ЦП"""
         return self.thread_manager.maxThreadCount()
 
-
-class DebugThread(QThread):
-    """Поток для дебага"""
-    cpu_disc_usage_signal = Signal(str)
-
-    def __init__(self, disc: str) -> None:
-        super().__init__()
-        self.disc = disc
-
-    def run(self) -> None:
-        """Подсчет использования CPU и диска"""
-        while True:
-            cpu_usage = psutil.cpu_percent()
-            disc_usage = psutil.disk_usage(self.disc)
-            disc_free = round(disc_usage.free / (1024 ** 3), 1)
-            disc_percent = disc_usage[3]
-            time.sleep(5)
-            self.cpu_disc_usage_signal.emit(f"Cpu usage: {cpu_usage}%, Disk free: {disc_free}GB,"
-                                            f" Disk usage: {disc_percent}%")
