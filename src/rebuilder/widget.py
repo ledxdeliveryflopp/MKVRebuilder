8569995d3e8728360866547a94e7988665edb124
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-13 21:27:28 +0700

    Обновлено окно сборки.
    1. Исправлены ошибки в строках.
    2. Добавлено дополнительное логирование.
    3. Добавлена функция старта профилирования.
    4. В все функции добавлена функция старта профилирования.
    5. Создание дочернего процесса при конвертации dts в ac3 заменено с subprocess.run на subprocess.popen, с запуском профилирования и ожидания завершения процесса.

--- src/rebuilder/widget.py
+++ src/rebuilder/widget.py
@@ -5,6 +5,7 @@ from time import time
 from PySide6 import QtWidgets, QtCore
 from loguru import logger
 
+from src.profiler.service import ProfilerThread
 from src.rebuilder.utils import generate_temp_dir, clear_temp_dts, clear_temp_ac3, clear_temp_srt, clear_temp_eac3
 from src.rebuilder.widget_ui import UiRebuilderWidget
 from src.settings.settings import settings
@@ -16,7 +17,7 @@ from static.mkv_tools.utils import get_mkv_tools_path
 class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
     """Виджет извлечения/конвертации/сборки"""
 
-    def __init__(self, main_window: QtWidgets.QMainWindow,
+    def __init__(self, main_window,
                  source_path: str, output_file: str, track_data: dict, subtitle_data: dict | None, temp_path: str,
                  bitrate: int, restricted_codec: str):
         super().__init__()
@@ -26,6 +27,7 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         self.track_data: dict = track_data
         self.subtitle_data: dict | None = subtitle_data
         self.temp_path: str = temp_path
+        self.profiler: ProfilerThread | None = ProfilerThread(None)
         self.restricted_codec: str = restricted_codec
         self.bitrate: int = bitrate
 
@@ -71,7 +73,7 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         self.ui.mkv.setText(self.tr("build new mkv: Not started"))
         self.setWindowFlags(QtCore.Qt.WindowType.WindowMinimizeButtonHint)
         if self.restricted_codec:
-            self.ui.ac3.setText(self.tr("extract subtitle: Skip"))
+            self.ui.ac3.setText(self.tr("convert dts to ac3: Skip"))
             self.ui.ac3.setProperty("skip", True)
         else:
             self.ui.ac3.setText(self.tr("convert dts to ac3: Not started"))
@@ -80,6 +82,7 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         else:
             self.ui.subtitle.setText(self.tr("extract subtitle: Skip"))
             self.ui.subtitle.setProperty("skip", True)
+        logger.info("Rebuilder translate applied")
 
     @logger.catch
     def set_mkv_ac3_tools_path(self) -> None:
@@ -89,6 +92,8 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         self.mkv_extract_path = fr"{mkv_tools_path}\mkvextract.exe"
         self.mkv_merge_path = fr"{mkv_tools_path}\mkvmerge.exe"
         self.ac3_converter = fr"{eac3_path}\eac3to.exe"
+        logger.info(f"Mkv/eac3to path set: mkvextract - {self.mkv_extract_path}, mvkmerge - {self.mkv_merge_path},"
+                    f" eac3to - {self.ac3_converter}")
 
     @logger.catch
     def set_black_style(self) -> None:
@@ -110,7 +115,16 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         else:
             self.set_standard_style()
 
-    def showEvent(self, event):
+    @logger.catch
+    def run_profiler(self, process_id: int) -> None:
+        """Запуск потока профилирования"""
+        if settings.get_profiler_status is True:
+            self.profiler.process_id = process_id
+            self.profiler.profiler_signal.connect(self.main_window.statusBar().showMessage)
+            self.profiler.start()
+
+    def showEvent(self, event) -> None:
+        """Выполнение функций при открытии виджета"""
         self.main_window.setEnabled(False)
         self.start_work()
 
@@ -170,19 +184,18 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
     @logger.catch
     def start_work(self) -> None:
         """Начало работы"""
-        logger.info(f"restricted_codec = {self.restricted_codec}")
-        logger.info(f"sub data = {self.subtitle_data}")
-        logger.info(f"track data = {self.track_data}")
-        logger.info(f"bitrate = {self.bitrate}")
         self.set_data()
         self.set_temp_dir()
         self.set_sound_track_path()
         self.set_subtitle_path()
         if self.restricted_codec == "AC-3":
+            logger.info("Start works with AC-3")
             self.thread_manager.start(self.extract_ac3_track)
         elif self.restricted_codec == "E-AC-3":
+            logger.info("Start works with E-AC-3")
             self.thread_manager.start(self.extract_eac3_track)
         else:
+            logger.info("Start works with DTS")
             self.thread_manager.start(self.extract_dts_track)
 
     @logger.catch
@@ -192,6 +205,7 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         command = subprocess.Popen([f"{self.mkv_extract_path}", "tracks", self.source_path, self.extract_ac3_track_path],
                                    stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
         logger.info(f"{self.extract_ac3_track.__name__}: {command.stdout.readline()}")
+        self.run_profiler(command.pid)
         for line in command.stdout:
             self.ui.sound.setText(self.tr(f"extract sound: {line}"))
         t2 = time()
@@ -210,6 +224,7 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         command = subprocess.Popen([f"{self.mkv_extract_path}", "tracks", self.source_path, self.extract_dts_track_path],
                                    stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
         logger.info(f"{self.extract_dts_track.__name__}: {command.stdout.readline()}")
+        self.run_profiler(command.pid)
         for line in command.stdout:
             self.ui.sound.setText(self.tr(f"extract sound: {line}"))
         t2 = time()
@@ -228,6 +243,7 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         command = subprocess.Popen([f"{self.mkv_extract_path}", "tracks", self.source_path, self.extract_eac3_track_path],
                                    stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
         logger.info(f"{self.extract_eac3_track.__name__}: {command.stdout.readline()}")
+        self.run_profiler(command.pid)
         for line in command.stdout:
             self.ui.sound.setText(self.tr(f"extract sound: {line}"))
         t2 = time()
@@ -246,6 +262,7 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         command = subprocess.Popen([f"{self.mkv_extract_path}", "tracks", self.source_path, self.extract_srt_path],
                                    stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
         logger.info(f"{self.extract_sub.__name__}: {command.stdout.readline()}")
+        self.run_profiler(command.pid)
         for line in command.stdout:
             self.ui.subtitle.setText(self.tr(f"extract subtitle: {line}"))
         t2 = time()
@@ -265,8 +282,10 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         t1 = time()
         logger.info(f"ac3 path - {self.ac3_path}")
         self.ui.ac3.setText(self.tr("converting dts to ac3"))
-        subprocess.run([f"{self.ac3_converter}", self.dts_path, self.ac3_path, f"-{self.bitrate}", f"-down6"],
-                       stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+        command = subprocess.Popen([f"{self.ac3_converter}", self.dts_path, self.ac3_path, f"-{self.bitrate}", f"-down6"],
+                                   text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+        self.run_profiler(command.pid)
+        command.wait()
         t2 = time()
         summary_time = t2 - t1
         elapsed = summary_time / 60
@@ -281,8 +300,10 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         logger.info("start rebuild with E-AC-3")
         if self.subtitle_data:
             sub_command = f'--language 0:{self.subtitle_lang} --track-name 0:"{self.subtitle_name}"'
-            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path} {sound_metadata} {self.eac3_path} {sub_command} {self.subtitle_path}',
+            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path}'
+                                       f' {sound_metadata} {self.eac3_path} {sub_command} {self.subtitle_path}',
                                        stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            self.run_profiler(command.pid)
             for line in command.stdout:
                 self.ui.mkv.setText(self.tr(f"rebuild mkv: {line}"))
             t2 = time()
@@ -293,8 +314,10 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
             clear_temp_eac3(self.eac3_path)
             clear_temp_srt(self.subtitle_path)
         else:
-            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path} {sound_metadata} {self.eac3_path}',
+            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path}'
+                                       f' {sound_metadata} {self.eac3_path}',
                                        stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            self.run_profiler(command.pid)
             for line in command.stdout:
                 self.ui.mkv.setText(self.tr(f"rebuild mkv: {line}"))
             t2 = time()
@@ -312,8 +335,10 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
         logger.info("start rebuild with AC-3")
         if self.subtitle_data:
             sub_command = f'--language 0:{self.subtitle_lang} --track-name 0:"{self.subtitle_name}"'
-            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path} {sound_metadata} {self.ac3_path} {sub_command} {self.subtitle_path}',
+            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path}'
+                                       f' {sound_metadata} {self.ac3_path} {sub_command} {self.subtitle_path}',
                                        stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            self.run_profiler(command.pid)
             for line in command.stdout:
                 self.ui.mkv.setText(self.tr(f"rebuild mkv: {line}"))
             t2 = time()
@@ -325,8 +350,10 @@ class RebuilderWidget(QtWidgets.QWidget, ThreadManager):
             clear_temp_ac3(self.ac3_path)
             clear_temp_srt(self.subtitle_path)
         else:
-            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path} {sound_metadata} {self.ac3_path}',
+            command = subprocess.Popen(f'{self.mkv_merge_path} -o {self.output_file} -A -S {self.source_path}'
+                                       f' {sound_metadata} {self.ac3_path}',
                                        stdout=subprocess.PIPE, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            self.run_profiler(command.pid)
             for line in command.stdout:
                 self.ui.mkv.setText(self.tr(f"rebuild mkv: {line}"))
             t2 = time()
